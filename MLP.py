1edf176d1d85b800b38f51171ecb8cf414c9f67e
Matthew <wppmat001@myuct.ac.za>
2023-04-27 15:02:05 +0200

    added template MLP code

---
+++ MLP.py
@@ -0,0 +1,111 @@
+import torch  # Main Package
+import torchvision  # Package for Vision Related ML
+import torchvision.transforms as transforms  # Subpackage that contains image transforms
+
+# Create the transform sequence
+transform = transforms.Compose([
+    transforms.ToTensor(),  # Convert to Tensor
+    # Normalize Image to [-1, 1] first number is mean, second is std deviation
+    transforms.Normalize((0.5,), (0.5,)) 
+])
+
+# Load MNIST dataset
+# Train
+trainset = torchvision.datasets.MNIST(root='./data', train=True,
+                                      download=True, transform=transform)
+# Test
+testset = torchvision.datasets.MNIST(root='./data', train=False,
+                                      download=True, transform=transform)
+
+# Send data to the data loaders
+BATCH_SIZE = 128
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
+                                          shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
+                                          shuffle=False)
+
+import torch.nn as nn  # Layers
+import torch.nn.functional as F # Activation Functions
+
+# Define the MLP architecture
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.flatten = nn.Flatten() # For flattening the 2D image
+        self.fc1 = nn.Linear(28*28, 512)  # Input is image with shape (28x28)
+        self.fc2 = nn.Linear(512, 256)  # First HL
+        self.fc3= nn.Linear(256, 10) # Second HL
+        self.output = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+      # Batch x of shape (B, C, W, H)
+      x = self.flatten(x) # Batch now has shape (B, C*W*H)
+      x = F.relu(self.fc1(x))  # First Hidden Layer
+      x = F.relu(self.fc2(x))  # Second Hidden Layer
+      x = self.fc3(x)  # Output Layer
+      x = self.output(x)  # For multi-class classification
+      return x  # Has shape (B, 10)
+# Identify device
+device = ("cuda" if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+# Creat the model and send its parameters to the appropriate device
+mlp = MLP().to(device)
+
+import torch.optim as optim # Optimizers
+
+# Define the training and testing functions
+def train(net, train_loader, criterion, optimizer, device):
+    net.train()  # Set model to training mode.
+    running_loss = 0.0  # To calculate loss across the batches
+    for data in train_loader:
+        inputs, labels = data  # Get input and labels for batch
+        inputs, labels = inputs.to(device), labels.to(device)  # Send to device
+        optimizer.zero_grad()  # Zero out the gradients of the ntwork i.e. reset
+        outputs = net(inputs)  # Get predictions
+        loss = criterion(outputs, labels)  # Calculate loss
+        loss.backward()  # Propagate loss backwards
+        optimizer.step()  # Update weights
+        running_loss += loss.item()  # Update loss
+    return running_loss / len(train_loader)
+def test(net, test_loader, device):
+    net.eval()  # We are in evaluation mode
+    correct = 0
+    total = 0
+    with torch.no_grad():  # Don't accumulate gradients
+        for data in test_loader:
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device) # Send to device
+            outputs = net(inputs)  # Get predictions
+            _, predicted = torch.max(outputs.data, 1)  # Get max value
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()  # How many are correct?
+    return correct / total
+mlp = MLP().to(device)
+
+LEARNING_RATE = 1e-4
+MOMENTUM = 0.9
+
+# Define the loss function, optimizer, and learning rate scheduler
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(mlp.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+
+# Train the MLP for 5 epochs
+for epoch in range(5):
+    train_loss = train(mlp, train_loader, criterion, optimizer, device)
+    test_acc = test(mlp, test_loader, device)
+    print(f"Epoch {epoch+1}: Train loss = {train_loss:.4f}, Test accuracy = {test_acc:.4f}")
+
+# Test on a batch of data
+with torch.no_grad():  # Don't accumlate gradients
+  mlp.eval()  # We are in evalutation mode
+  x = example_data.to(device)
+  outputs = mlp(x)  # Alias for mlp.forward
+
+  # Print example output.
+  print(torch.exp(outputs[0]))
+  print(f'Prediction: {torch.max(outputs, 1)[1][0]}')
