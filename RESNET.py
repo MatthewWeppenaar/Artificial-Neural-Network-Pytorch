1cfa851e0155a9b589d316620f45a4b3f5c15f09
Matthew <wppmat001@myuct.ac.za>
2023-06-17 02:41:57 +0200

    Adding simplified version of ResNet9(one residual block) to RESNET.py

--- RESNET.py
+++ RESNET.py
@@ -31,7 +31,6 @@ test_loader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
 examples = enumerate(test_loader)
 batch_idx, (example_data, example_targets) = next(examples)
 
-print(example_data.shape)
 
 import torch.nn as nn  # Layers
 import torch.nn.functional as F # Activation Functions
@@ -39,80 +38,56 @@ import torch.nn.functional as F # Activation Functions
 
 
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
-        super(ResidualBlock, self).__init__()
+class SimpleResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
         self.conv1 = nn.Sequential(
-                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
-                        nn.BatchNorm2d(out_channels),
+                        nn.Conv2d(128,128, kernel_size = 3, stride = 1, padding = 1),
+                        nn.BatchNorm2d(128),
                         nn.ReLU())
         self.conv2 = nn.Sequential(
-                        nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
-                        nn.BatchNorm2d(out_channels))
-        self.downsample = downsample
+                        nn.Conv2d(128, 128, kernel_size = 3, stride = 1, padding = 1),
+                        nn.BatchNorm2d(128))
+        
         self.relu = nn.ReLU()
-        self.out_channels = out_channels
+        
         
     def forward(self, x):
         residual = x
         out = self.conv1(x)
         out = self.conv2(out)
-        if self.downsample:
-            residual = self.downsample(x)
         out += residual
         out = self.relu(out)
         return out
 
-
-
 class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes = 10):
-        super(ResNet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Sequential(
-                        nn.Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3),
-                        nn.BatchNorm2d(64),
-                        nn.ReLU())
-        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
-        self.layer0 = self._make_layer(block, 64, layers[0], stride = 1)
-        #self.layer1 = self._make_layer(block, 128, layers[1], stride = 2)
-        ##self.layer2 = self._make_layer(block, 256, layers[2], stride = 2)
-        #self.layer3 = self._make_layer(block, 512, layers[3], stride = 2)
-        #self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(4096,128)
-        self.fc2 = nn.Linear(128, num_classes)
-        
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes:
-            
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(planes),
-            )
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels,64, kernel_size=3, padding=1)
+        self.conv1_bn = nn.BatchNorm2d(64)
+        self.pool = nn.MaxPool2d(2)
 
-        return nn.Sequential(*layers)
-    
-    
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.maxpool(x)
-        x = self.layer0(x)
-       # x = self.layer1(x)
-       # x = self.layer2(x)
-       # x = self.layer3(x)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3,padding=1)
+        self.conv2_bn = nn.BatchNorm2d(128)
 
-        #x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        x = self.fc2(x)
+        #creating a simple residual block
+        self.res1 = SimpleResidualBlock()
 
-        return x
+        self.pool2 = nn.MaxPool2d(4)
+        self.fc1 = nn.Linear(2048,512)
+        self.fc2 = nn.Linear(512,num_classes)
+        self.flatten = nn.Flatten()
+ 
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.pool(F.relu(out))
+        out = self.res1(out)
+        out = self.pool2(out)
+        out = self.flatten(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
 
 
 device = ("cuda" if torch.cuda.is_available()
@@ -152,18 +127,16 @@ def test(net, test_loader, device):
             correct += (predicted == labels).sum().item()  # How many are correct?
     return correct / total
 
-model = ResNet(ResidualBlock, [1, 1, 1, 1]).to(device)
+model = ResNet(3, 10).to(device)
 
 LEARNING_RATE = 1e-3
 MOMENTUM = 0.9
 
 # Define the loss function, optimizer, and learning rate scheduler
 criterion = nn.CrossEntropyLoss()
-''', momentum=MOMENTUM'''
-optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-lr_decay = optim.lr_scheduler.StepLR(optimizer,5,0.1)
-lr_decay2 = optim.lr_scheduler.StepLR(optimizer,8,0.2)
-lr_decay3 = optim.lr_scheduler.StepLR(optimizer,15,0.2)
+optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+lr_decay = optim.lr_scheduler.StepLR(optimizer,10,0.1)
+
 
 import sys
 
@@ -171,12 +144,10 @@ import sys
 if sys.argv[1] == "-save":
     test_mlp = []
     test_scores = []    
-    for epoch in range(20):
+    for epoch in range(15):
         train_loss = train(model, train_loader, criterion, optimizer, device)
         test_acc = test(model, test_loader, device)
         lr_decay.step()
-        lr_decay2.step()
-        lr_decay3.step()
         print(f"Epoch {epoch+1}: Train loss = {train_loss:.4f}, Test accuracy = {test_acc:.4f}")
         test_mlp.append(model.state_dict())
         test_scores.append(test_acc)
@@ -188,7 +159,7 @@ if sys.argv[1] == "-save":
     max_index = test_scores.index(max(test_scores))
     torch.save(test_mlp[max_index], "best_RES.pth")
 if sys.argv[1] == '-load':
-    res = ResNet(ResidualBlock, [1, 1, 1, 1])
+    res = ResNet(3, 10).to(device)
     print("loading params...")
     res.load_state_dict(torch.load("best_RES.pth"))
     print("Done !")
